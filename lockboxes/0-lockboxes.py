815f5888d1e72c60345ee80bc2d10059e5b73570
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-03-01 23:53:07 +0400

    done

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,23 +5,12 @@
 
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-
-    all_keys = set(range(1, len(boxes)))
-
-    opened_boxes = set()
-    stack = [0]
-
-    while stack:
-        current_box = stack.pop()
-        opened_boxes.add(current_box)
-
-        all_keys -= set(boxes[current_box])
-
-        for key in boxes[current_box]:
-            if key not in opened_boxes and key < len(boxes):
-                stack.append(key)
-
-    return len(all_keys) == 0
-
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+    return True
