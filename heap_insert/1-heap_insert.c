23ac49e4d81cf3cd8077d4d10fd2fbf8b94228ca
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-03-14 20:53:56 +0400

    done

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,21 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * heapify_up - Restores the Max Heap property after insertion
+ * find_parent - Find the parent for the new node
+ * @root: Pointer to the root node
+ * Return: Pointer to the parent node
+ */
+heap_t *find_parent(heap_t *root)
+{
+    while (root && root->left)
+    {
+        if (!root->right)
+            return (root);
+        root = root->left;
+    }
+    return (root);
+}
+
+/**
+ * heapify_up - Heapify up after insertion
  * @node: Pointer to the newly inserted node
  */
 void heapify_up(heap_t *node)
 {
-    int temp;
-    heap_t *parent;
-
     while (node->parent && node->n > node->parent->n)
     {
-        parent = node->parent;
-        temp = parent->n;
-        parent->n = node->n;
-        node->n = temp;
-        node = parent;
+        int temp = node->n;
+        node->n = node->parent->n;
+        node->parent->n = temp;
+        node = node->parent;
     }
 }
 
@@ -30,39 +42,19 @@ heap_t *heap_insert(heap_t **root, int value)
 {
     heap_t *new_node, *parent;
 
-    /* Create a new node */
     new_node = binary_tree_node(NULL, value);
-    if (new_node == NULL)
+    if (!new_node)
         return (NULL);
 
-    /* If the heap is empty, the new node becomes the root */
-    if (*root == NULL)
-    {
-        *root = new_node;
-        return (new_node);
-    }
+    if (!*root)
+        return (*root = new_node);
 
-    /* Find the parent of the new node */
-    parent = *root;
-    while (parent->left != NULL && parent->right != NULL)
-    {
-        if (parent->left != NULL && parent->right != NULL)
-            parent = parent->left;
-        else
-            parent = parent->right;
-    }
-
-    /* Insert the new node as left child if the parent's left child is NULL */
-    if (parent->left == NULL)
+    parent = find_parent(*root);
+    if (!parent->left)
         parent->left = new_node;
-    /* Otherwise, insert the new node as right child */
     else
         parent->right = new_node;
-
-    /* Set the parent of the new node */
     new_node->parent = parent;
-
-    /* Restore the Max Heap property */
     heapify_up(new_node);
 
     return (new_node);
