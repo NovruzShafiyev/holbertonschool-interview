eae597b0cb1d8f7f36160741d6faf998fdb90730
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-03-20 18:58:53 +0400

    done

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-def minOperations(n):
+"""
+Module to calculate the minimum number of operations required to achieve
+a specific number of characters in a file using the Copy All and Paste operations.
+"""
+
+
+def min_operations(n):
+    
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
 
