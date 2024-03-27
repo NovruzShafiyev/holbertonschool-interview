2f6b629a8047285798d7e077c6e3064244c849f5
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-03-27 23:48:56 +0400

    done

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,12 +1,6 @@
-#ifndef _SANDPILES_H_
-#define _SANDPILES_H_
+#ifndef SANDPILES_H
+#define SANDPILES_H
 
-#include <stddef.h>
-
-
-static void print_grid(int grid[3][3]);
-
-
-int stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-#endif /* _SANDPILES_H_ */
+
+#endif /* SANDPILES_H */
