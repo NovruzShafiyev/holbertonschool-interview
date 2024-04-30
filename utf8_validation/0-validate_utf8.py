b3ad6aebddee79003876759389a8702b957cd460
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-01 03:41:16 +0400

    done

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
-"""
-Determines if a given data set represents a valid UTF-8 encoding.
-"""
+""" method that determines valid UTF-8 encoding """
+
+
+def EightBits(integer):
+    """ A function that:
+    converts an integer to its binary representation """
+    binary = bin(integer)[2:]
+    add = '0' * (8 - len(binary))
+    return add + binary
+
 
 def validUTF8(data):
-    """
-    Determines if a given data set represents a valid UTF-8 encoding.
-    """
-    bytes_to_follow = 0
-
-    for byte in data:
-        if bytes_to_follow == 0:
-            if byte >> 7 == 0:
-                bytes_to_follow = 0
-            elif byte >> 5 == 0b110:
-                bytes_to_follow = 1
-            elif byte >> 4 == 0b1110:
-                bytes_to_follow = 2
-            elif byte >> 3 == 0b11110:
-                bytes_to_follow = 3
-            else:
-                return False
+    """ method that checks a valid UTF-8 """
+    i = 0
+    while i < len(data):
+        if EightBits(data[i])[0] == '0':
+            i += 1
+        elif (EightBits(data[i])[:3] == '110' and i + 1 < len(data) and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 2
+        elif (EightBits(data[i])[:4] == '1110' and i + 2 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 1])[:2] == '10'):
+            i += 3
+        elif (EightBits(data[i])[:5] == '11110' and i + 3 < len(data) and
+                EightBits(data[i + 1])[:2] == '10' and
+                EightBits(data[i + 2])[:2] == '10' and
+                EightBits(data[i + 3])[:2] == '10'):
+            i += 4
         else:
-            if byte >> 6 != 0b10:
-                return False
-            bytes_to_follow -= 1
-            if bytes_to_follow < 0:
-                return False
-
-    return bytes_to_follow == 0
-
-if __name__ == "__main__":
-    data1 = [65]
-    print(validUTF8(data1))
-
-    data2 = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data2))
-
-    data3 = [229, 65, 127, 256]
-    print(validUTF8(data3))
+            return False
+    return True
