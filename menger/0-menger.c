c979847f188a1417a12f356a5cc4ce62535c83c7
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-05-20 18:20:53 +0400

    done

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,47 +1,51 @@
+#include "menger.h"
 #include <stdio.h>
 #include <math.h>
-#include "menger.h"
 
 /**
- * is_menger_hole - Checks if the given position (x, y) is part of the hole.
- * @x: The x coordinate.
- * @y: The y coordinate.
- *
- * Return: 1 if it is a hole, otherwise 0.
+ * menger - draws a 2D Menger Sponge
+ * @level: level of the Menger Sponge to draw
+ * Return: No return
  */
-int is_menger_hole(int x, int y)
-{
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (1);
-        x /= 3;
-        y /= 3;
-    }
-    return (0);
-}
 
-/**
- * menger - Draws a 2D Menger Sponge of a given level.
- * @level: The level of the Menger Sponge to draw.
- *
- * Return: void
- */
 void menger(int level)
 {
-    if (level < 0)
-        return;
+	int size, width;
+	int i = 0, j = 0;
 
-    int size = pow(3, level);
-    for (int y = 0; y < size; y++)
-    {
-        for (int x = 0; x < size; x++)
-        {
-            if (is_menger_hole(x, y))
-                putchar(' ');
-            else
-                putchar('#');
-        }
-        putchar('\n');
-    }
+
+	size = pow(3, level);
+
+	if (level == 0)
+		printf("#\n");
+	else
+	{
+		while (i < size)
+		{
+			while (j < size)
+			{
+				width = size / 3;
+				while (width >= 1)
+				{
+					if (((i / width) % 3 == 1 && (j / width) % 3 == 1))
+					{
+						printf(" ");
+						break;
+					}
+					else if (i % 3 == 1 && j % 3 == 1)
+					{
+						printf(" ");
+						break;
+					}
+					width /= 3;
+				}
+				if (width == 0)
+					printf("#");
+				j++;
+			}
+			printf("\n");
+			i++;
+			j = 0;
+		}
+	}
 }
