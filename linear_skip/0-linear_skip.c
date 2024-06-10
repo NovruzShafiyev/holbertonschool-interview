55b3cd858bfa382c7eb5498ba09c15bc49d2ae75
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-06-10 20:59:12 +0400

    done

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -10,38 +10,38 @@
  */
 skiplist_t *linear_skip(skiplist_t *head, int value)
 {
-    skiplist_t *node = head, *express = NULL;
+	skiplist_t *node = head, *express = NULL;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    while (node && node->express && node->express->n < value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
-        node = node->express;
-    }
+	while (node && node->express && node->express->n < value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
+		node = node->express;
+	}
 
-    if (node && node->express)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
-        express = node->express;
-    }
-    else
-    {
-        express = node;
-        while (express->next)
-            express = express->next;
-    }
+	if (node && node->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", node->express->index, node->express->n);
+		express = node->express;
+	}
+	else
+	{
+		express = node;
+		while (express->next)
+			express = express->next;
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n", node->index, express->index);
+	printf("Value found between indexes [%lu] and [%lu]\n", node->index, express->index);
 
-    while (node && node->index <= express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-        if (node->n == value)
-            return (node);
-        node = node->next;
-    }
+	while (node && node->index <= express->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+		if (node->n == value)
+			return (node);
+		node = node->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
