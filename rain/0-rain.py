295269d3c592be22b5123446feee7a11f49013fb
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-06-27 23:08:11 +0400

    done

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -36,9 +36,3 @@ def rain(walls):
         water_retained += max(0, min(left_max[i], right_max[i]) - walls[i])
     
     return water_retained
-
-if __name__ == "__main__":
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))
-    walls = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls))
